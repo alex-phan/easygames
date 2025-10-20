19e43b413396e5c4506e3c5842246107946b224b
Gislene FDLClancy <s361662@cdu.edu.au>
2025-10-20 17:30:18 +1000

    chore: remove duplicate Veasna_Parts tree (keep Email Groups in canonical paths)

--- Veasna_Parts/easygames-main/ViewModels/ProductVMs.cs
+++
@@ -1,4 +0,0 @@
-﻿// list vms see Notepad v1
-public class ProductRowVM { public int Id; public string Title = ""; public string Category = ""; public decimal Price; }
-public class ProductListVM { public IEnumerable<ProductRowVM> Items { get; set; } = new List<ProductRowVM>(); public string? Search { get; set; } }
-
